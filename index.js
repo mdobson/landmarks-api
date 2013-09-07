c57c3c5bc45f1474117e0819baaedf522ac53a74
Matt Dobson <mdobson4@gmail.com>
2013-09-07 15:33:03 -0400

    deploy time.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var argo = require("argo"),
     landmarks = require("./landmarks.json");
 
 argo()
-  .route('/landmarks', function(handle){
+  .route('/', function(handle){
     handle('request', function(env, next){
      env.response.statusCode = 200;
      env.response.body = landmarks;
